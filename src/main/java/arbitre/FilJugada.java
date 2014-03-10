8b66dbeb10672a0167c6b247a884eda9707b0046
gienini <Alumno22@SBS-ALUMNO22>
2014-03-10 17:17:30 +0100

    Afegit fil per executar jugades

---
+++ src/main/java/arbitre/FilJugada.java
@@ -0,0 +1,32 @@
+package arbitre;
+
+import jugadors.Jugable;
+
+public class FilJugada extends Thread {
+	private int x;
+	private int y;
+	private int z;
+	private int torn;
+	private Jugable jugador;
+	private Jugada retorn;
+
+	public FilJugada(int x, int y, int z, int torn, Jugable jugador) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.torn = torn;
+		this.jugador = jugador;
+	}
+	
+	public void run() {
+		retorn=jugador.fesJugada(x, y, z, torn);
+	}
+	
+	@Override
+	public void interrupt() {
+		retorn = new Jugada(0, true, true, true);
+		System.out.println("El jugador "+jugador.getClass()+" ha tardat massa a pensar");
+		super.interrupt();
+	}
+}
